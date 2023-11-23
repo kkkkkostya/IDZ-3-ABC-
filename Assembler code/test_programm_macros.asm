52188f3c0231246a6677f46e8e837fb3f306a63b
Kostya <71220034+kkkkkostya@users.noreply.github.com>
2023-11-23 23:17:12 +0300

    Update test_programm_macros.asm

--- Assembler code/test_programm_macros.asm
+++ Assembler code/test_programm_macros.asm
@@ -2,31 +2,31 @@
 
 .global test_programm_macros
 .text
-test_programm_macros:							# Òåñòîâàÿ ïðîãðàììà
+test_programm_macros:							# Тестовая программа
 	li s10, 0
 	li s11, 2
 	
-	addi sp sp -4		# Çàïàñàåì îäíó ÿ÷åéêó äëÿ ñîõðàíåíèÿ ra
-	sw ra (sp)			# Ñîõðàíÿåì ra
+	addi sp sp -4		# Запасаем одну ячейку для сохранения ra
+	sw ra (sp)			# Сохраняем ra
 	loopp:
 	
-		call_read_file()			# Ââîä èìåíè ôàéëà
+		call_read_file()			# Ввод имени файла
 	
 		la a0, strbuf
 		
-		call_string_processing		# Îáðàáîòêà ñ÷èòàííîé ñòðîêè
+		call_string_processing		# Обработка считанной строки
 		
 		print_or_not(t1)
 		
 		la a6, new_str
-		call_create_new_file 		# Ñîçäàíèå íîâîãî ôàéëà
+		call_create_new_file 		# Создание нового файла
 	
 		addi s10,s10,1
 		beq s10,  s11,ex
 		j loopp
 		
 	ex:
-		lw ra (sp)		# âîññòàíàâëèâàåì ra èç ñòåêà
-		addi sp sp 4	# âîññòàíàâëèâàåì âåðøèíó ñòåêà
+		lw ra (sp)		# восстанавливаем ra из стека	
+		addi sp sp 4		# восстанавливаем вершину стека
 		ret
 		
